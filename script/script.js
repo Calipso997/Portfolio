a1a731aaebf275b3fd5fe40a05f7a5fb96a80f91
Calipso997 <marekkamzol@tlen.pl>
2017-08-21 17:18:12 +0200

    my first commit

---
+++ script/script.js
@@ -0,0 +1,20 @@
+window.onscroll = function () {
+   scrollFunction()
+};
+
+function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("myBtn").style.display = "block";}
+   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10){
+//      document.getElementById("gora").classList.add("fixed-top");
+      document.getElementById("gora").classList.add("trans");
+   } else {
+      document.getElementById("myBtn").style.display = "none";
+//      document.getElementById("gora").classList.remove("fixed-top");
+      document.getElementById("gora").classList.remove("trans");
+   }
+};
+function CValert() {
+    document.getElementById("CVclick")
+     alert("Cv przesyłam po kontakcie meilowym marekkamzol@tlen.pl");
+}
